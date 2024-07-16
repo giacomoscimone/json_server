15d2410e363022375ed92d9ac4c3a60ff9bc0d97
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-16 12:28:34 +0200

    delete main

--- base_main.py
+++
@@ -1,8 +0,0 @@
-import requests  # è necessario installare la libreria
-
-base_url = "https://gnews.io/api/v4/top-headlines"
-
-response = requests.get(url)
-articles = response.json()["articles"]
-
-
